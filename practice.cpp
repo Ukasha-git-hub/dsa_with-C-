93dc5a0a2e6a0cdea8db08db33937dacd607f398
Ukasha-git-hub <ukashakhursheed@gmail.com>
2025-07-30 21:41:10 +0500

    revivsion

--- practice.cpp
+++ practice.cpp
@@ -4,18 +4,35 @@
 #include <cmath>
 
 using namespace std;
- void p (double x, int n){
-    
-double fv = pow(x,n);
-    cout<< fv<< endl;
+//  void p (vector<int> &nums){
+//   for(int i=0; i< nums.size(); i++){
+//    for(int j=i+1;j<nums.size();j++){
+//       int andval= nums[i];
+//       for(int k = i; k<= j; k++ ){
+//         int out = andval & nums[k];
+//         cout<<out<< " ";
+      
+//       }
+   
+//    }
+//      cout <<endl;
+//   }  
+  
+//  }
+ int p1(double x, int n){
+ int   max = 1;
+   for(int i = 0; i< n ;i++){
+       max *=  x;
+
+   }
+   return max;
  }
 int main()
 {
-   // double x =4;
-   // double n = 4;
-   double x = 0;
-   int n = 3;
-    p(x,n);
+  //vector<int> nums = {1,2,3,4};
+  double x= 5;
+  int n = -3;
+ cout << p1(x,n) <<endl;
    return 0;
 }
 //pow(x, n)
